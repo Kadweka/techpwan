a121764512330e0dbe182ac3a9b12dd01276942e
hussein18149 <husseinkatana17@gmail.com>
2020-02-07 15:34:43 +0300

    add styling and the type script of the page

---
+++ javascript/enyewe.js
@@ -0,0 +1,10 @@
+
+  var typed = new Typed('.moringa', {
+    strings: ["A TECH CENTER",
+    "To bring professional developers",
+    "What ideas can you bring on the table?",
+    "SORRY THE WEBSITE IS UNDER-DEVELOPMENT!!"],
+    typeSpeed:60,
+    backSpeed:60,
+    loop:true
+  });
